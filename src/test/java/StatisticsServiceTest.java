b28cd9db7a1feb9c778d3d903c8c82e9005d0e80
Sema test <ganzalovdmitriy@gmail.com>
2023-03-04 20:55:19 +0300

    add extra test

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.Assertions;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void findMaxWhenMaximumInBegin() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
@@ -16,5 +18,17 @@ public class StatisticsServiceTest {
         long actual = service.findMax(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWhenMaximumInMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 50, 39, 8, 6, 11, 11, 12};
+        long expected = 50;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
     }
 }
